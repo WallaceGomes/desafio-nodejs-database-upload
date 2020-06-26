1e74015d67e57c530eb93b448ae56da26717b534
unknown <wallacecardosogomes@gmail.com>
2020-06-26 19:31:46 -0300

    chore: added migrations, models and repositories

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,37 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, type, value }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type invalid');
+    }
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total - value < 0) {
+      throw new Error('Not Able to create a new transaction');
+    }
+
+    const transaction = transactionsRepository.save({
+      title,
+      value,
+      type,
+    });
+
+    // falta checar a category
+
+    return transaction;
   }
 }
 
